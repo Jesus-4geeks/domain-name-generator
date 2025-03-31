3b024c4d6197a0c24410351436b3b38b8603ef08
Jesus-4Geeks <madafaking@PC-JML>
2025-03-31 20:38:55 +0200

    Updated project

--- src/app.js
+++ src/app.js
@@ -5,16 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-function excuseGenerator(variables) {
-  let excuse = "";
-
-  for (let v of variables) {
-    let randInt = Math.floor(Math.random() * v.length);
-    excuse += v[randInt] + ' ';
-  }
-
-  // Devolvemos a excusa sin el espacio al final
-  return excuse.trim();
+function partGenerator(variable) {
+  let randInt = Math.floor(Math.random() * variable.length);
+  return variable[randInt] + ' ';
 }
 
 window.onload = function() {
@@ -24,7 +17,12 @@ window.onload = function() {
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-  let variables = [who, action, what, when]
+  let excuse = "";
+
+  excuse += partGenerator(who);
+  excuse += partGenerator(action);
+  excuse += partGenerator(what);
+  excuse += partGenerator(when);
   
-  document.getElementById("excuse").innerHTML = excuseGenerator(variables);
+  document.getElementById("excuse").innerHTML = excuse.trim();
 };
